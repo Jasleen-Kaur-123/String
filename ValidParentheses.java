be0c75b2040d0947862871bb8aa723e08920f85e
Jasleen Kaur <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2025-04-10 08:30:53 +0000

    initial

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,5 +1,28 @@
+import java.util.Stack;
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello");
+        String p = "";
+        System.out.println(checkParentheses(p));
+    }
+    public static boolean checkParentheses(String p){
+        int len = p.length();
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<len;i++){
+            char single = p.charAt(i);
+            if(single == '(' || single == '{' || single == '['){
+                st.push(single);
+            }
+            else{
+                if(st.isEmpty()){
+                return false;
+                }
+                char top = st.pop();
+                if((single == '(' && top != ')') ||
+                  (single == '{' && top != '}') ||
+                  (single == '[' && top != ']'))
+                  return false; 
+            }
+        }
+        return st.isEmpty();
     }
 }
